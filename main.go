896690c22f157557e96aaff7f157c539d3984cf7
SaraTrawnik <saratrawnik@cock.li>
2019-02-17 16:37:53 +0100

    reads threads

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import(
 	"github.com/moshee/go-4chan-api/api"
+
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("init")
+	arguments = os.Args[1:]
+	board := arguments[0]
+	thread, err := strconv.ParseInt(arguments[1], 10, 64)
+	if err != nil { fmt.Println("invalid thread number"); return }
+	threadPosts := api.GetThread(board, thread)
+	for _, x := range thread.Posts {
+		fmt.Println(x.Id, x.Time, x.Subject, "\n",
+					x.Name, "\n",
+					x.Comment, "\n"
+				)
+	}
 }
